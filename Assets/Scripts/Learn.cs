8ae0e8ba8af60fae4402b5e58b1862345eb37517
xujintao23 <1468638461@qq.com>
2023-02-11 19:00:37 +0800

    modify code

--- Assets/Scripts/Learn.cs
+++ Assets/Scripts/Learn.cs
@@ -7,29 +7,8 @@ using UnityEngine;
 
 public class Learn : MonoBehaviour
 {
-     Type t = typeof(A);
-
-     void Start()
-     {
-         FieldInfo[] fi = t.GetFields();
-         MethodInfo[] mi = t.GetMethods();
-
-         foreach (var item in fi)
-             Debug.Log($"Filed:{item.Name}");
-         foreach (var item in mi)
-             Debug.Log($"Method:{item.Name}");
-     }
-}
-
-class  A
-{
-    public int Field;
-    public int Field1;
-    
-    public void Method1(){}
-
-    public int Method2()
+    private void Start()
     {
-        return 1;
+        Debug.Log("xxxx");
     }
 }
