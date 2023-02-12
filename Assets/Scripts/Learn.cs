4675a3e4feedb6d4a56b55841ca21fdd448a7c35
xujintao23 <1468638461@qq.com>
2023-02-12 22:17:00 +0800

    add test

--- Assets/Scripts/Learn.cs
+++ Assets/Scripts/Learn.cs
@@ -1,14 +1,9 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Reflection;
-using TMPro;
+
 using UnityEngine;
+
 
 public class Learn : MonoBehaviour
 {
-    private void Start()
-    {
-        Debug.Log("xxxx");
-    }
+
+    
 }
